21ba8d360ca3cc63b6814986075a666ccc417709
Vadim Smagin <magenvadim@gmail.com>
2023-11-09 23:49:00 +0200

    #2 header.css was added

--- src/Header.js
+++ src/Header.js
@@ -3,11 +3,14 @@ import './Header.css'
 function Header() {
   return (
     <div className="container">
+      <header>
         <div className="notif_box">
-            <h2 className="title">Notifications</h2>
-            <span id="notifications"></span>
-        </div>
-        <p id="mark_all">Mark all as read</p>
+              <h2 className="title">Notifications</h2>
+              <span id="notifications"></span>
+          </div>
+          <p id="mark_all">Mark all as read</p>
+      </header>
+
     </div>
 
   )
